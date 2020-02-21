f86caa77f33d01c11b1ff8f3b96f8e56bef38192
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-02-21 11:33:21 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,7 @@
 
   def age_appropriate_birthday(birthday_kids)
     birthday_kids.each do |kids_name, age|
-      if name == < 12
+      if name  < 12
         puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
       end
     end
